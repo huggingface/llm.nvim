c4395707742a8bfd21ab3c4c9edd6ce4a9112259
Luc Georges <luc.sydney.georges@gmail.com>
2023-05-09 09:52:54 +0200

    feat: move cursor position at end of suggestion

--- lua/hfcc/completion.lua
+++ lua/hfcc/completion.lua
@@ -13,9 +13,13 @@ function M.complete()
 
   hf.fetch_suggestion({ before = before, after = after }, function(response, r, _)
     local lines = utils.split_str(response, "\n")
+    local lines_len = utils.table_len(lines)
     local line = api.nvim_buf_get_lines(0, r - 1, r, false)[1]
     lines[1] = line .. lines[1]
+    local row_offset = r + lines_len - 1
+    local col_offset = string.len(lines[lines_len]) - 1
     api.nvim_buf_set_lines(0, r - 1, r, false, lines)
+    api.nvim_win_set_cursor(0, { row_offset, col_offset })
   end)
 end
 
