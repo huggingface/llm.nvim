3e1afe63b46a0b3d5b661c1c42bdd4220caac5cb
Luc Georges <luc.sydney.georges@gmail.com>
2023-09-21 14:31:37 +0200

    feat: bump llm-ls to `0.1.1`

--- lua/llm/config.lua
+++ lua/llm/config.lua
@@ -25,7 +25,7 @@ local default_config = {
   ---@class llm_config_lsp
   lsp = {
     bin_path = nil,
-    version = "0.1.0",
+    version = "0.1.1",
   },
   tokenizer_path = nil,
   context_window = 8192,
