c34047fd8ac87d6e0475eb0a96a6f8772a27a971
adamthedash <adamthedash2@gmail.com>
2024-11-06 22:54:19 +0000

    add default llamacpp config

--- lua/llm/config.lua
+++ lua/llm/config.lua
@@ -51,7 +51,10 @@ local default_request_bodies = {
     temperature = 0.2,
     top_p = 0.95,
   },
-  llamacpp = {},
+  llamacpp = {
+    tmperature = 0.2,
+    top_p = 0.95,
+  },
 }
 
 local M = {
